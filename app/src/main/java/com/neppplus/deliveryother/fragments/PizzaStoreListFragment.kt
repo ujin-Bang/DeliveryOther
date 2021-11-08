007ac4aa1255b20584bb96e0f8c1e0660e6e69ac
ujin-Bang <qkddnwls14@gmail.com>
2021-11-08 12:02:55 +0900

    피자가게 목록 프래그먼트 xml, kt 연결

---
+++ app/src/main/java/com/neppplus/deliveryother/fragments/PizzaStoreListFragment.kt
@@ -0,0 +1,21 @@
+package com.neppplus.deliveryother.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.deliveryother.R
+
+class PizzaStoreListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_pizza_store_list, container, false)
+
+    }
+}
