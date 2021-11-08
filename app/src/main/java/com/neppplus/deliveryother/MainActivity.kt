24bf1facf0c1053b27e0349d9f0c5a58ef07712e
ujin-Bang <qkddnwls14@gmail.com>
2021-11-08 13:21:05 +0900

    점심먹으러가기전 save

--- app/src/main/java/com/neppplus/deliveryother/MainActivity.kt
+++ app/src/main/java/com/neppplus/deliveryother/MainActivity.kt
@@ -2,10 +2,17 @@ package com.neppplus.deliveryother
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.deliveryother.adapters.MainViewPagerAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mvpa : MainViewPagerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+    set
 }
