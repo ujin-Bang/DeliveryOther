5f2c963741632d2ac97ec20ef4ae878a65638603
ujin-Bang <qkddnwls14@gmail.com>
2021-11-08 14:53:11 +0900

    피자가게 어댑터 기초작업 마무리

--- app/src/main/java/com/neppplus/deliveryother/adapters/PizzaStoreAdapter.kt
+++ app/src/main/java/com/neppplus/deliveryother/adapters/PizzaStoreAdapter.kt
@@ -1,11 +1,33 @@
 package com.neppplus.deliveryother.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.deliveryother.R
 import com.neppplus.deliveryother.datas.StoreData
 
 class PizzaStoreAdapter(
     val mContext: Context,
-    val resId: Int,
+    resId: Int,
     val mList: ArrayList<StoreData> ) : ArrayAdapter<StoreData>(mContext,resId,mList) {
+
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null ) {
+
+            tempRow = mInflater.inflate(R.layout.store_list_item,null)
+
+
+        }
+        val row = tempRow!!
+
+        return row
+
+    }
 }
